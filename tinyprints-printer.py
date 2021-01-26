d9f6580a0a15090a26d26da2b18a2b2f63983146
stefanavra <stefan.avra@gmail.com>
2021-01-27 00:04:45 +0100

    added the "if name is main" thing

--- tinyprints-printer.py
+++ tinyprints-printer.py
@@ -7,10 +7,7 @@ LOCAL = 'http://127.0.0.1:5000/api/'
 API_KEY = os.environ.get('TINYPRINTS_API_PASSWORD') or 'dev'
 
 
-URL
-
 ser = serial.Serial('/dev/serial0')
-
 
 
 def get_winner():
@@ -23,32 +20,22 @@ def close(pw):
     return r
 
 
-r = close(API_KEY)
-print(r.raise_for_status())
+if __name__ == "__main__":
+    r = close(API_KEY)
+    print(r.raise_for_status())
 
-r = get_winner()
-print(r.raise_for_status())
+    r = get_winner()
+    print(r.raise_for_status())
 
+    tinyprint = r.json()
+    print(tinyprint)
 
-tinyprint = r.json()
-print(tinyprint)
-# {
-#     'created': 'Mon, 18 Jan 2021 00:28:41 GMT',
-#     'id': 4,
-#     'text': '\n\n _._     _,-\'""`-._\r\n(,-.`._,\'(       |\\`-/|\r\n    `-.-\' \\ )-`( , o o)\r\n          `-    \\`_`"\'-',
-#     'title': 'sneaking cat',
-#     'votes': 2,
-#     'voting_closed_timestamp': 'Tue, 26 Jan 2021 22:20:34 GMT'
-# }
-
-
-ser.write(b'\x1b\x21\x00') # reset
-ser.write(tinyprint.get('title').encode())
-ser.write('\n'.encode())
-ser.write(tinyprint.get('text').encode())
-ser.write('\n'.encode())
-ser.write('#{}  upvotes: {}\n'.format(tinyprint.get('id'), tinyprint.get('votes')).encode())
-ser.write(tinyprint.get('created').encode())
-ser.write('\n\n\n'.encode())
-
-
+    ser.write(b'\x1b\x21\x00')  # reset
+    ser.write(tinyprint.get('title').encode())
+    ser.write('\n'.encode())
+    ser.write(tinyprint.get('text').encode())
+    ser.write('\n'.encode())
+    ser.write('#{}  upvotes: {}\n'.format(
+        tinyprint.get('id'), tinyprint.get('votes')).encode())
+    ser.write(tinyprint.get('created').encode())
+    ser.write('\n\n\n'.encode())
